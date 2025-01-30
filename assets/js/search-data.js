5633a350f839062400c147d6891b350404edcd18
marywu-12 <marywu2026@u.northwestern.edu>
2025-01-30 09:14:57 +0000

    Deploying to gh-pages from @ marywu-12/marywu-12.github.io@e764c195885b4aabf3830e33db10ab67f06822d3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "nav-courses",
           title: "courses",
           description: "Select courses that I&#39;ve taken and enjoyed. Feel free to use my notes as references!",
