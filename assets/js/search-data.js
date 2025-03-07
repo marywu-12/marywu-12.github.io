f1a1e34facf4aafde7a1a6d6365f434fa95c1ab3
marywu-12 <marywu2026@u.northwestern.edu>
2025-03-07 21:16:41 +0000

    Deploying to gh-pages from @ marywu-12/marywu-12.github.io@4b3781754eb2d4aec02c0c40e0d28e8897f5f598 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -312,17 +312,6 @@ ninja.data = [{
           window.location.href = "/blog/2020/twitter/";
         
       },
-    },{id: "post-a-post-with-math",
-      
-        title: "a post with math",
-      
-      description: "an example of a blog post with some math",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2015/math/";
-        
-      },
     },{id: "news-i-graduated-from-new-york-university",
           title: 'I graduated from New York University.',
           description: "",
